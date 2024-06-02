5f81c51e4fae1f63e2aef48137e2d7d3cc138175
afrolino02 <andres.puello.gomez@gmail.com>
2024-06-02 12:52:21 -0500

    codigo java implementado para operaciones crud

---
+++ src/main/java/com/model/Usuario.java
@@ -0,0 +1,107 @@
+package com.model;
+
+public class Usuario {
+    private int id;
+    private int idNumber;
+    private String idType;
+    private String name;
+    private String lastName;
+    private String dateBirth;
+    private String email;
+    private String genred;
+    private String phone;
+    private int usrType;
+    
+    public Usuario(){}
+
+    public Usuario(int id,int idNumber,String idType,String name,String lastName, String dateBirth, String genred, String email, String phone,int usrType){
+        this.id = id;
+        this.idNumber = idNumber;
+        this.idType = idType;
+        this.name = name;
+        this.lastName = lastName;
+        this.dateBirth = dateBirth;
+        this.genred = genred;
+        this.email = email;
+        this.phone = phone;
+        this.usrType = usrType;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getIdNumber() {
+        return this.idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getIdType() {
+        return this.idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateBirth() {
+        return this.dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+    public String getGender() {
+        return this.genred;
+    }
+
+    public void setGender(String genred) {
+        this.genred = genred;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getUsrType(){
+        return this.usrType;
+    }
+
+    public void setUsrType(int usrType){
+        this.usrType = usrType;
+    }
+}
